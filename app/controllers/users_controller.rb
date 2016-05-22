06b95f5aeccab6a45fbc49f4e9cab85f84a8612a
Vivian Li <vivooo@gmail.com>
2016-05-22 17:21:31 +1000

    finished user controller and added home page and pages controller added root path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,16 @@ class UsersController < ApplicationController
   end
 
   def edit
+  @user = @current_user
   end
 
   def update
+    @user = @current_user
+    if @user.update user_params
+      redirect_to root_path
+    else
+    render :edit
+    end
   end
 
   def new
