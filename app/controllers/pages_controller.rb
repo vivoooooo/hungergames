c72c30f2071e8760720ddbdb03ed3f05fcb081d7
twillzy <william.tanzy@gmail.com>
2016-05-22 23:12:56 +1000

    session done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,2 @@
 class PagesController < ApplicationController
-  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise 'hell'
     @user = User.new user_params
     if @user.save
       redirect_to root_path
