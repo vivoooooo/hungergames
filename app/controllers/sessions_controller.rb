c9c2b766b6f6c7c9505da98c07f8fa9380547e4f
Vivian Li <vivooo@gmail.com>
2016-05-26 19:40:56 +1000

    fixed seed file and added link in home page and got rid of raises

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    raise 'hell'
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
       log_in user
@@ -18,3 +17,22 @@ class SessionsController < ApplicationController
     redirect_to root_path
   end
 end
+
+
+
+
+#   def create
+#     user = User.find_by :email => params[:email]
+#     if user.present? && user.authenticate(params[:password])
+#       session[:user_id] = user.id
+#       redirect_to root_path
+#     else
+#       redirect_to login_path
+#     end
+#   end
+
+#   def destroy
+#     session[:user_id] = nil
+#     redirect_to root_path
+#   end
+# end
